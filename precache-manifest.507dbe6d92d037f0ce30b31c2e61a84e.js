192c8a62ae75561ae2699e6ef3a857213e5d344f
Elise Welch <eliseanyawelch@gmail.com>
2020-06-25 15:36:19 -0500

    Updates

---
+++ precache-manifest.507dbe6d92d037f0ce30b31c2e61a84e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "336f60d9defd1068fd16e2418c5a7f07",
+    "url": "/elise-portfolio/index.html"
+  },
+  {
+    "revision": "1b0137770552e0049ed0",
+    "url": "/elise-portfolio/static/css/main.64051354.chunk.css"
+  },
+  {
+    "revision": "5829fb859e439275db80",
+    "url": "/elise-portfolio/static/js/2.08fb700a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/elise-portfolio/static/js/2.08fb700a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b0137770552e0049ed0",
+    "url": "/elise-portfolio/static/js/main.2b631d32.chunk.js"
+  },
+  {
+    "revision": "5cda2fa108db21114a01",
+    "url": "/elise-portfolio/static/js/runtime-main.aa978af2.js"
+  },
+  {
+    "revision": "628a52eb7064259835540886d15f6094",
+    "url": "/elise-portfolio/static/media/elise.628a52eb.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/elise-portfolio/precache-manifest.507dbe6d92d037f0ce30b31c2e61a84e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/elise-portfolio/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
